89de960fca665d15098382d577ed1ed0e0ed0557
khoituanphan <tuankhoi.phan0820@gmail.com>
2023-08-23 11:44:59 +0700

    Img, hover box

--- src/page-layouts/home-page/HomePageTest.js
+++ src/page-layouts/home-page/HomePageTest.js
@@ -4,34 +4,34 @@
 import classes from "../../../styles/HomePageTest.module.css";
 import React from "react";
 import { useRouter } from "next/navigation";
-import Image from 'next/image';
-
+import Image from "next/image";
+import user from "../../theme/home_page/user.png";
+import threedots from "../../theme/home_page/threedots.png";
+import recent from "../../theme/home_page/recent.png";
+import playlist from "../../theme/home_page/playlist.png";
+import home from "../../theme/home_page/home.png";
+import history from "../../theme/home_page/history.png";
+import ARIS_Logo from "../../theme/home_page/logo.png";
 
 function HomePageTest() {
   const router = useRouter();
 
-  const navigateToAnotherPage = () => {
+  const modelViewer = () => {
     router.push("/upload");
   };
 
   return (
     <section>
-      <header>
-        <h1>My Header</h1> {/* Apply CSS class */}
-        <button onClick={navigateToAnotherPage}>
-          Navigate to Another Page
-        </button>
-      </header>
       <section className={classes.main_container}>
         <section className={classes.main_content}>
           <h1 className={classes.section_title}>Home</h1>
           <section className={classes.main_grid}>
             <section className={classes.main_thumbnail}>
-              <section className={classes.thumbnail_box}></section>
+              <button onClick={modelViewer} className={classes.thumbnail_box}/>
             </section>
             <section className={classes.main_canvas_heading}>
               <h2 className={classes.main_text}>
-                <span>Nightly News Full Broadcast -</span>
+                <span>ModelViewer</span>
                 <br />
                 <span>Aug. 13</span>
               </h2>
@@ -52,7 +52,7 @@ function HomePageTest() {
           <h1 className={classes.section_title}>Recent</h1>
           <section className={classes.main_grid}>
             <section className={classes.main_thumbnail}>
-              <section className={classes.thumbnail_box}></section>
+              <section className={classes.recentthumbnail_box}></section>
             </section>
             <section className={classes.main_canvas_heading}>
               <h2 className={classes.main_text}>
@@ -64,7 +64,7 @@ function HomePageTest() {
           </section>
           <section className={classes.main_grid}>
             <section className={classes.main_thumbnail}>
-              <section className={classes.thumbnail_box}></section>
+              <section className={classes.recentthumbnail_box}></section>
             </section>
             <section className={classes.main_canvas_heading}>
               <h2 className={classes.main_text}>
@@ -81,7 +81,7 @@ function HomePageTest() {
           <section className={classes.main_end}>
             <button className={classes.main_banner_options}>
               <Image
-                src="../../theme/home_page/threedots.svg"
+                src={threedots}
                 className={classes.main_button_settings}
                 width={40}
                 height={40}
@@ -90,23 +90,27 @@ function HomePageTest() {
             </button>
             <button className={classes.main_sign_in_box}>
               <Image
-                src="../../theme/home_page/user.svg"
-                className={classes.main_yticon}
-                width={40}
-                height={40}
-                alt="Sign In"
+                src={user}
+                className={classes.main_button_settings}
+                width={24}
+                height={24}
+                alt="user"
               />
               <span className={classes.nav_text}>
-                <span>Sign up</span>
+                <span>Profile</span>
               </span>
             </button>
             <section className={classes.main_divlogo}>
               <Image
                 alt="ARIS_Logo"
-                src="/logo.png"
+                src={ARIS_Logo}
                 className={classes.main_bannerlogo}
-                width={40}
-                height={40}
+                width={80}
+                height={80}
+                style={{
+                  maxWidth: "100%",
+                  height: "auto",
+                }}
               />
             </section>
           </section>
@@ -117,7 +121,7 @@ function HomePageTest() {
           <section id="nav-grid-upper" className={classes.main_nav_grid_upper}>
             <section className={classes.nav_box}>
               <Image
-                src="public/external/yticonyticonshapeiconshapesvg3117-oaiv.svg"
+                src={home}
                 className={classes.main_yticon}
                 alt="Home"
                 width={24}
@@ -129,7 +133,7 @@ function HomePageTest() {
             </section>
             <section className={classes.nav_box}>
               <Image
-                src="public/external/yticonyticonshapeiconshapesvg3118-q1q.svg"
+                src={playlist}
                 className={classes.main_yticon}
                 alt="Shorts"
                 width={24}
@@ -141,19 +145,7 @@ function HomePageTest() {
             </section>
             <section className={classes.nav_box}>
               <Image
-                src="public/external/yticonyticonshapeiconshapesvg3119-bkoo.svg"
-                className={classes.main_yticon}
-                alt="Subscriptions"
-                width={24}
-                height={24}
-              />
-              <h3 className={classes.nav_text}>
-                <span>Subscriptions</span>
-              </h3>
-            </section>
-            <section className={classes.nav_box}>
-              <Image
-                src="public/external/yticonyticonshapeiconshapesvg3119-bk0e.svg"
+                src={recent}
                 className={classes.main_yticon}
                 alt="Library"
                 width={24}
@@ -165,7 +157,7 @@ function HomePageTest() {
             </section>
             <section className={classes.nav_box}>
               <Image
-                src="public/external/yticonyticonshapeiconshapesvg3120-ks2a.svg"
+                src={history}
                 className={classes.main_yticon}
                 alt="History"
                 width={24}

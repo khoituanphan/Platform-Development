77f9517782b3c356196817fa14d9b3cc2d2ceae3
kahnbt <leminhkhanhs1012@gmail.com>
2023-10-21 10:55:03 +0700

    Yarn build okay

--- app/profile/page.js
+++ app/profile/page.js
@@ -4,10 +4,16 @@ import { authOptions } from '../api/auth/[...nextauth]/route';
 import UserProfile from '@/src/profile/UserProfile';
 import { redirect } from 'next/navigation';
 
-export default async function ProfilePage() {
-	const session = getServerSession(authOptions);
-	if (!session) {
-		redirect('/auth');
-	}
-	return <UserProfile />;
+export default function ProfilePage({ session }) {
+    if (!session) {
+        redirect('/auth');
+    }
+    return <UserProfile />;
+}
+
+export async function getServerSideProps(context) {
+    const session = await getServerSession(authOptions);
+    return {
+        props: { session }
+    };
 }

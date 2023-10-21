458c4fc0260e5a8d9da531b94c9c1ef343c7b177
kahnbt <leminhkhanhs1012@gmail.com>
2023-10-21 13:09:00 +0700

    vercel

--- app/profile/page.js
+++ app/profile/page.js
@@ -3,18 +3,20 @@ import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 import UserProfile from '@/src/profile/UserProfile';
 import { redirect } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
-export default function Profile({ session }) {
+export default function Profile() {
+    const { data: session } = useSession();
     if (!session) {
         redirect('/auth');
     }
     return <UserProfile />;
 }
 
-export async function getServerSideProps(context) {
-    const session = await getServerSession(authOptions);
-    return {
-        props: { session }
-    };
-}
+// export async function getServerSideProps(context) {
+//     const session = await getServerSession(authOptions);
+//     return {
+//         props: { session }
+//     };
+// }
 

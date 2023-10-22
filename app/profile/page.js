05208d9fe2f24916d05606dd4f62d0b09d78eab3
therealozp <anhkhang.le0910@gmail.com>
2023-10-22 01:53:56 -0400

    final fixes on ProfileForm (async and use client issues)

--- app/profile/page.js
+++ app/profile/page.js
@@ -4,8 +4,8 @@ import { authOptions } from '../api/auth/[...nextauth]/route';
 import UserProfile from '@/src/profile/UserProfile';
 import { redirect } from 'next/navigation';
 
-export default function ProfilePage() {
-	const session = getServerSession(authOptions);
+export default async function ProfilePage() {
+	const session = await getServerSession(authOptions);
 	if (!session) {
 		redirect('/auth');
 	}

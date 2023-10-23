d2ab17caf1b2ce24bbc82962c6f5d61cc858a33c
therealozp <anhkhang.le0910@gmail.com>
2023-10-23 11:39:00 -0400

    remove unneeded session command

--- app/api/register-beta/route.js
+++ app/api/register-beta/route.js
@@ -6,7 +6,6 @@ import { NextResponse } from 'next/server';
 async function POST(req) {
 	try {
 		const request = await req.json();
-		console.log('from /api/user/models/route.js: ', session);
 		const client = await clientPromise;
 		const db = client.db();
 		const betaCollection = db.collection('betas');

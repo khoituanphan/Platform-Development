304e083e506531ff939d9f7780228b7ddc9c4126
therealozp <anhkhang.le0910@gmail.com>
2023-10-23 11:37:43 -0400

    register-beta path

---
+++ app/api/register-beta/route.js
@@ -0,0 +1,30 @@
+// app/api/get-uploaded-model.js
+
+import clientPromise from '@/lib/mongoClient';
+import { NextResponse } from 'next/server';
+
+async function POST(req) {
+	try {
+		const request = await req.json();
+		console.log('from /api/user/models/route.js: ', session);
+		const client = await clientPromise;
+		const db = client.db();
+		const betaCollection = db.collection('betas');
+
+		const fileResponse = await betaCollection.insertOne({
+			email: request.email,
+		});
+
+		return NextResponse.json({
+			status: 200,
+			body: { message: 'User added to beta list.' },
+		});
+	} catch {
+		return NextResponse.json({
+			status: 500,
+			body: { message: 'Internal server error' },
+		});
+	}
+}
+
+export { POST };

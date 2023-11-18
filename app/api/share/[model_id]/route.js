e9b1ac2fe9bc5b120cb2182b12b3c57a360f9418
therealozp <anhkhang.le0910@gmail.com>
2023-11-18 13:48:42 -0500

    hotfix api

--- app/api/share/[model_id]/route.js
+++ app/api/share/[model_id]/route.js
@@ -1,8 +1,8 @@
 import clientPromise from '@/lib/mongoClient';
 import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
-import { authOptions } from '../auth/[...nextauth]/route';
 import { ObjectId } from 'mongodb';
+import { authOptions } from '../../auth/[...nextauth]/route';
 
 async function POST(req) {
 	try {

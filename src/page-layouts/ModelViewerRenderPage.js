3dd846c4f59fe6f4e8bf9f6ba739accab4647f54
kahnbt <leminhkhanhs1012@gmail.com>
2023-11-19 00:19:50 +0700

    Update ModelViewerRenderPage.js

--- src/page-layouts/ModelViewerRenderPage.js
+++ src/page-layouts/ModelViewerRenderPage.js
@@ -96,7 +96,7 @@ const FloatingExport = ({ modelId, onExport }) => {
 										<InputGroup>
 											<Input
 												readOnly
-												value={`https://localhost:3000/share/${modelId}`}
+												value={`https://platform-development-phi.vercel.app/share/${modelId}`}
 												ref={inputref}
 											/>
 											<InputRightAddon as="button" onClick={copy}>

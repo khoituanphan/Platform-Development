b56462352d714186e00daedaec57f7b430dd5f9d
kahnbt <leminhkhanhs1012@gmail.com>
2023-11-21 17:55:44 +0700

    Update route.js

--- app/api/user/models/route.js
+++ app/api/user/models/route.js
@@ -28,49 +28,30 @@ import { ObjectId } from 'mongodb';
 // 	return res.status(200).json({ modelUrl: fileData.url });
 // }
 
+
 async function POST(req) {
-	// try {
-	const session = await getServerSession(authOptions);
-	if (!session) {
-		return NextResponse.json({
-			status: 401,
-			body: { message: 'No session found' },
-		});
-	}
-	const request = await req.json();
-	console.log('from /api/user/models/route.js: ', session);
-	const client = await clientPromise;
-	const db = client.db();
-	const fileCollection = db.collection('files');
+    const request = await req.json();
+    const client = await clientPromise;
+    const db = client.db();
+    const fileCollection = db.collection('files');
 
-	const fileResponse = await fileCollection.findOne({
-		_id: new ObjectId(request.fileID),
-	});
+    const fileResponse = await fileCollection.findOne({
+        _id: new ObjectId(request.fileID),
+    });
 
-	if (session?.user.email !== fileResponse.belongsTo) {
-		return NextResponse.json({
-			status: 401,
-			body: { message: 'You are not authorized to view this model' },
-		});
-	}
+    if (!fileResponse) {
+        return NextResponse.json({
+            status: 404,
+            body: { message: 'No uploaded model found' },
+        });
+    }
 
-	if (!fileResponse) {
-		return NextResponse.json({
-			status: 404,
-			body: { message: 'No uploaded model found' },
-		});
-	}
-
-	return NextResponse.json({
-		status: 200,
-		body: { modelUrl: fileResponse.url },
-	});
-	// } catch {
-	// 	return NextResponse.json({
-	// 		status: 500,
-	// 		body: { message: 'Internal server error' },
-	// 	});
-	// }
+    return NextResponse.json({
+        status: 200,
+        body: { modelUrl: fileResponse.url },
+    });
 }
 
 export { POST };
+
+

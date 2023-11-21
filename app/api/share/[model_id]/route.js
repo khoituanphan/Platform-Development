5c6e4f0d6baf4d362be7e349d6061c75777573bd
kahnbt <leminhkhanhs1012@gmail.com>
2023-11-21 18:30:22 +0700

    Revert "Update route.js"
    This reverts commit 736bea414b4b3b08ff722948124f279059b0e749.

--- app/api/share/[model_id]/route.js
+++ app/api/share/[model_id]/route.js
@@ -1,35 +1,46 @@
 import clientPromise from '@/lib/mongoClient';
-import { ObjectId } from 'mongodb';
+import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
+import { ObjectId } from 'mongodb';
+import { authOptions } from '../../auth/[...nextauth]/route';
 
 async function POST(req) {
-    try {
-        const request = await req.json();
-        const client = await clientPromise;
-        const db = client.db();
-        const fileCollection = db.collection('files');
+	try {
+		const session = await getServerSession(authOptions);
+		if (!session) {
+			return NextResponse.json({
+				status: 401,
+				body: { message: 'No session found' },
+			});
+		}
+		console.log('from /api/user/models/route.js: ', session);
 
-        const fileResponse = await fileCollection.findOne({
-            _id: new ObjectId(request.fileID),
-        });
+		const request = await req.json();
+		const client = await clientPromise;
+		const db = client.db();
+		const fileCollection = db.collection('files');
 
-        if (!fileResponse) {
-            return NextResponse.json({
-                status: 404,
-                body: { message: 'No uploaded model found' },
-            });
-        }
+		const fileResponse = await fileCollection.findOne({
+			_id: new ObjectId(request.fileID),
+		});
 
-        return NextResponse.json({
-            status: 200,
-            body: { modelUrl: fileResponse.url },
-        });
-    } catch {
-        return NextResponse.json({
-            status: 500,
-            body: { message: 'Internal server error' },
-        });
-    }
+		if (!fileResponse) {
+			return NextResponse.json({
+				status: 404,
+				body: { message: 'No uploaded model found' },
+			});
+		}
+
+		return NextResponse.json({
+			status: 200,
+			body: { modelUrl: fileResponse.url },
+		});
+	} catch {
+		return NextResponse.json({
+			status: 500,
+			body: { message: 'Internal server error' },
+		});
+	}
 }
 
 export { POST };

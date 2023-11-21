736bea414b4b3b08ff722948124f279059b0e749
kahnbt <leminhkhanhs1012@gmail.com>
2023-11-21 17:49:11 +0700

    Update route.js

--- app/api/share/[model_id]/route.js
+++ app/api/share/[model_id]/route.js
@@ -1,46 +1,35 @@
 import clientPromise from '@/lib/mongoClient';
-import { getServerSession } from 'next-auth';
-import { NextResponse } from 'next/server';
 import { ObjectId } from 'mongodb';
-import { authOptions } from '../../auth/[...nextauth]/route';
+import { NextResponse } from 'next/server';
 
 async function POST(req) {
-	try {
-		const session = await getServerSession(authOptions);
-		if (!session) {
-			return NextResponse.json({
-				status: 401,
-				body: { message: 'No session found' },
-			});
-		}
-		console.log('from /api/user/models/route.js: ', session);
+    try {
+        const request = await req.json();
+        const client = await clientPromise;
+        const db = client.db();
+        const fileCollection = db.collection('files');
 
-		const request = await req.json();
-		const client = await clientPromise;
-		const db = client.db();
-		const fileCollection = db.collection('files');
+        const fileResponse = await fileCollection.findOne({
+            _id: new ObjectId(request.fileID),
+        });
 
-		const fileResponse = await fileCollection.findOne({
-			_id: new ObjectId(request.fileID),
-		});
+        if (!fileResponse) {
+            return NextResponse.json({
+                status: 404,
+                body: { message: 'No uploaded model found' },
+            });
+        }
 
-		if (!fileResponse) {
-			return NextResponse.json({
-				status: 404,
-				body: { message: 'No uploaded model found' },
-			});
-		}
-
-		return NextResponse.json({
-			status: 200,
-			body: { modelUrl: fileResponse.url },
-		});
-	} catch {
-		return NextResponse.json({
-			status: 500,
-			body: { message: 'Internal server error' },
-		});
-	}
+        return NextResponse.json({
+            status: 200,
+            body: { modelUrl: fileResponse.url },
+        });
+    } catch {
+        return NextResponse.json({
+            status: 500,
+            body: { message: 'Internal server error' },
+        });
+    }
 }
 
 export { POST };

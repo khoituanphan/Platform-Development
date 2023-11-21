ace039d2dd2801b3ed7d142221c3b1950eac7ce3
kahnbt <leminhkhanhs1012@gmail.com>
2023-11-21 23:29:23 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    modularizeImports: {
+        'antd/lib/QRCode': {
+            transform: 'antd/lib/qrcode',
+        },
+    },
+}
 module.exports = nextConfig

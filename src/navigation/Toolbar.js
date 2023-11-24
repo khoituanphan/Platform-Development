418b8e5f4b21fbb88af1f992d350cfa9be7e88a4
kahnbt <leminhkhanhs1012@gmail.com>
2023-11-24 22:06:27 +0700

    Update Toolbar.js

--- src/navigation/Toolbar.js
+++ src/navigation/Toolbar.js
@@ -20,17 +20,17 @@ const PanelButtons = ({ children, tooltip, hoverStyle, icon ,...props }) => {
           variant='outline'
         //   minWidth="60px" // Adjust this value as needed
           margin="0 10px"
-          color="white" // Set text color to white
+          color="black" // Set text color to white
           _hover={{ 
             bg: "blue.500", // Change background to blue on hover
-            color: "white", // Keep text color white on hover
+            color: "black", // Keep text color white on hover
             ...hoverStyle,
             border: 'none' // Removes border on hover as well
         }}      
           border="none"
         >
             <Flex alignItems="center" justifyContent="center">
-            {icon ? <Box as={icon} ml="0.5" color="black" /> : null}
+            {icon ? <Box as={icon} ml="0.5" color="black" /> : null}        
             {children}
             </Flex>
         </Button>
@@ -39,80 +39,94 @@ const PanelButtons = ({ children, tooltip, hoverStyle, icon ,...props }) => {
 };
 
 const Toolbar = ({ onSaveToLocal, onInitializeFromLocal, onClearLocal, onExport }) => {
-    const { addModel } = useModelStateStore();
-    const fileInputRef = useRef();
+  const { addModel } = useModelStateStore();
+  const fileInputRef = useRef();
 
+  const handleUpload = (e) => {
+      const uuid = uuidv4();
+      const file = e.target.files[0];
+      const fileURL = URL.createObjectURL(file);
+      addModel(fileURL, uuid);
+  };
 
-    const handleUpload = (e) => {
-        const uuid = uuidv4();
-        const file = e.target.files[0];
-        const fileURL = URL.createObjectURL(file);
-
-        addModel(fileURL, uuid);
-        // additional logic if needed
-    };
-
-    const triggerFileInput = () => {
-        fileInputRef.current.click();
-    };
+  const triggerFileInput = () => {
+      fileInputRef.current.click();
+  };
 
   return (
-    <Flex
-      position="fixed"
-      width="1000px"
-      height="calc(100vh - 1050px)"
-      left="600"
-      margin="32px"
-      zIndex={99}
-      boxShadow={'0px 0px 10px 0px rgba(0,0,0,0.75)'}
-      bgColor={'#292d39'}
-      borderRadius={'12px'}
-      alignItems={'center'}
-      flexDirection={'row'}
-      justifyContent='space-between' // Adjusts the space between the left and right groups
-      top="10px"
-    >
-      <Flex> {/* Group for left buttons */}
-        <PanelButtons 
-            onClick={onSaveToLocal} 
-            tooltip="Save to local"
-            icon={ArrowDownIcon}
-        /> 
-        <PanelButtons 
-            onClick={onInitializeFromLocal} 
-            tooltip="Load data from local" 
-            icon={DownloadIcon}
-        />
-        <PanelButtons 
-            onClick={triggerFileInput} 
-            tooltip="Add a model"
-            icon={AddIcon}
-        />
-        <PanelButtons 
-            onClick={onClearLocal} 
-            tooltip="Clear scene"
-            icon={DeleteIcon}
-        />
-      </Flex>
+      <>
+          <Flex
+              visibility="hidden" // Hide the toolbar background
+              position="fixed"
+              width="1000px"
+              height="calc(100vh - 1050px)"
+              left="600"
+              margin="32px"
+              zIndex={98} // Lower zIndex so the buttons appear on top
+              boxShadow={'0px 0px 10px 0px rgba(0,0,0,0.75)'}
+              bgColor={'#292d39'}
+              borderRadius={'12px'}
+              top="10px"
+          >
+              {/* Toolbar background (hidden) */}
+          </Flex>
 
-      <Flex> {/* Group for right button */}
-        <PanelButtons 
-            onClick={onExport}
-            tooltip="Export "
-            hoverStyle={{ bg: "blue.500", color: "white" }}>Export</PanelButtons>
-      </Flex>
-      <FormLabel htmlFor="toolbar-model-upload" hidden>
-                <Input
-                    ref={fileInputRef}
-                    hidden
-                    accept=".glb, .gltf"
-                    type="file"
-                    id="toolbar-model-upload"
-                    onChange={handleUpload}
-                />
-            </FormLabel>
-    </Flex>
+          <Flex
+              position="fixed"
+              left="600"
+              top="10px"
+              margin="32px"
+              zIndex={99} // Ensure buttons are on top of the hidden toolbar
+              justifyContent='space-between'
+              width="1000px" // Match the width of the toolbar for proper alignment
+          >
+              {/* Visible buttons */}
+              <Flex> {/* Group for left buttons */}
+                  <PanelButtons 
+                      onClick={onSaveToLocal} 
+                      tooltip="Save to local"
+                      icon={ArrowDownIcon}
+                  /> 
+                  <PanelButtons 
+                      onClick={onInitializeFromLocal} 
+                      tooltip="Load data from local" 
+                      icon={DownloadIcon}
+                  />
+                  <PanelButtons 
+                      onClick={triggerFileInput} 
+                      tooltip="Add a model"
+                      icon={AddIcon}
+                  />
+                  <PanelButtons 
+                      onClick={onClearLocal} 
+                      tooltip="Clear scene"
+                      icon={DeleteIcon}
+                  />
+              </Flex>
+
+              <Flex> {/* Group for right button */}
+                  <PanelButtons 
+                      onClick={onExport}
+                      tooltip="Export"
+                      hoverStyle={{ bg: "blue.500", color: "black" }}
+                  >
+                      Export
+                  </PanelButtons>
+              </Flex>
+          </Flex>
+          <FormLabel htmlFor="toolbar-model-upload" hidden>
+              <Input
+                  ref={fileInputRef}
+                  hidden
+                  accept=".glb, .gltf"
+                  type="file"
+                  id="toolbar-model-upload"
+                  onChange={handleUpload}
+              />
+          </FormLabel>
+      </>
   );
 };
 
 export default Toolbar;
+
